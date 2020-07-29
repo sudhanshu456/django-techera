e42574bb1fa04427fb78d1df20b1b5767e010770
sudhanshu456 <spsupraj2d@gmail.com>
2020-07-29 19:46:36 +0530

    day 2

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('list',views.index,name='index'),
+    path('',views.data,name='list'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Blogpost
 
 def index(request):
     return HttpResponse('<h1>hello world</h1>')
+
+
+def data(request):
+    queryset=Blogpost.objects.all()
+    context={
+        'data':queryset,
+    }
+    return render(request,'home.html',context)
+
 
 
