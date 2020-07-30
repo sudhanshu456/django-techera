96b536b79299be8399f7f6b9d8984d2d07fd3483
sudhanshu456 <spsupraj2d@gmail.com>
2020-07-30 19:24:37 +0530

    day 3

--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 from .models import Blogpost
 
 def index(request):
-    return HttpResponse('<h1>hello world</h1>')
+    
+    return render(request,'test.html')
 
 
 def data(request):
